91e3ed0666688acd31ac0f8edffc8ff824eec828
Andrii777555 <102090179+Andrii777555@users.noreply.github.com>
2022-03-21 23:23:22 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+class Block {
+constructor(index, previousHash, timestamp, data, hash) {
+
+this.index = index;
+
+this.previousHash = previousHash.toString();
+
+this.timestamp = timestamp;
+
+this.data = data;
+
+this.hash = hash.toString();
+
+}
+
+}
